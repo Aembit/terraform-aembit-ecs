c0d84379b358b34e18876f1f997574675bb1911a
Victor Ronin <victor.ronin@gmail.com>
2024-01-23 09:11:32 -0500

    Release 1.11.0 (#10)

--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "aembit_stack" {
 variable "agent_controller_image" {
   type = string
   description = "The container image to use for the AgentController installation"
-  default = "aembit/aembit_agent_controller:1.9.696"
+  default = "aembit/aembit_agent_controller:1.11.742"
 }
 
 variable "agent_proxy_image" {
