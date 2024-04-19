469d39a3026e45cf5a83d2c663d58277d3c9eb93
Victor Ronin <victor.ronin@gmail.com>
2024-04-19 12:22:37 -0400

    Release 1.13.2 (#17)
    Bump up Agent Proxy

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "agent_controller_image" {
 variable "agent_proxy_image" {
   type = string
   description = "The container image to use for the AgentProxy installation"
-  default = "aembit/aembit_agent_proxy:1.13.1818"
+  default = "aembit/aembit_agent_proxy:1.13.1851"
 }
 
 # ECS CLUSTER Specific Variables
