de983e997f6c491bd6908b6dd6c48733bd390ec6
Fatih Tan <fatihtan@users.noreply.github.com>
2025-04-15 15:26:59 +0300

    EDGEX-81 Allow HTTP to be disabled on Agent Controller to allow for collocation with other software that uses similar protocols on similar ports (#38)
    * EDGEX-81 Introduce environment variable AEMBIT_HTTP_PORT_DISABLED to ECS task definition
    
    * EDGEX-81 Update main.tf
    
    Co-authored-by: Drew Vogel <dvogel@aembit.io>
    
    ---------
    
    Co-authored-by: Fatih Tan <ftan@aembit.com>
    Co-authored-by: Drew Vogel <dvogel@aembit.io>

--- main.tf
+++ main.tf
@@ -50,7 +50,9 @@ resource "aws_ecs_task_definition" "agent-controller" {
       {"name": "AEMBIT_TENANT_ID", "value": var.aembit_tenantid },
       {"name": "AEMBIT_STACK_DOMAIN", "value": var.aembit_stack },
       {"name": "AEMBIT_AGENT_CONTROLLER_ID", "value": var.aembit_agent_controller_id },
-      {"name": "AEMBIT_MANAGED_TLS_HOSTNAME", "value": "${aws_service_discovery_service.agent-controller.name}.${aws_service_discovery_private_dns_namespace.agent-controller.name}"}
+      {"name": "AEMBIT_MANAGED_TLS_HOSTNAME", "value": "${aws_service_discovery_service.agent-controller.name}.${aws_service_discovery_private_dns_namespace.agent-controller.name}"},
+      {"name": "AEMBIT_HTTP_PORT_DISABLED", "value": tostring(var.aembit_http_port_disabled) }
+
     ]
     healthCheck = {
       retries = 6
