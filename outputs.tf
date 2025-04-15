0b2ec00c03aadc57714673e8b00d244dd1a4d508
Drew Vogel <dvogel@aembit.io>
2025-04-15 10:11:26 -0500

    Terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 locals {
   agent_proxy_default_environment_variables = {
-		"AEMBIT_AGENT_CONTROLLER": "https://${aws_service_discovery_service.agent-controller.name}.${aws_service_discovery_private_dns_namespace.agent-controller.name}:443",
-		"TRUSTED_CA_CERTS": local.all_trusted_ca_certs_base64,
-		"AEMBIT_RESOURCE_SET_ID": var.agent_proxy_resource_set_id,
-		"AEMBIT_AGENT_PROXY_DEPLOYMENT_MODEL": "ecs_fargate",
-	}
+    "AEMBIT_AGENT_CONTROLLER" : "https://${aws_service_discovery_service.agent-controller.name}.${aws_service_discovery_private_dns_namespace.agent-controller.name}:443",
+    "TRUSTED_CA_CERTS" : local.all_trusted_ca_certs_base64,
+    "AEMBIT_RESOURCE_SET_ID" : var.agent_proxy_resource_set_id,
+    "AEMBIT_AGENT_PROXY_DEPLOYMENT_MODEL" : "ecs_fargate",
+  }
   agent_proxy_effective_environment_variables = merge(local.agent_proxy_default_environment_variables, var.agent_proxy_environment_variables)
 }
 
@@ -30,8 +30,8 @@ output "agent_proxy_container" {
       }
     } : null)
     environment = [
-			for varname, varvalue in local.agent_proxy_effective_environment_variables : { "name": varname, "value": varvalue }
-		]
+      for varname, varvalue in local.agent_proxy_effective_environment_variables : { "name" : varname, "value" : varvalue }
+    ]
   })
 }
 
